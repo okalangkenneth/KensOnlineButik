13a01808015c14aa3700a51a44c468abb0ad3532
Kenneth Okalang <okalang.ok@gmail.com>
2022-10-13 09:05:14 +0200

    Add project files.

---
+++ KensOnlineButik/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace KensOnlineButik
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ KensOnlineButik/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(KensOnlineButik.Startup))]
+namespace KensOnlineButik
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
